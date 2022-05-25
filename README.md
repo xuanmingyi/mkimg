087a864f0386f567c0303045978ad5ca88d7586a
sin <xuanmingyi@gmail.com>
2022-05-26 06:31:18 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# 配置文件
+
+```yaml
+output: hello.img
+
+boot: boot.bin
+```
+
+* output 软驱镜像1440kb(1.44mb)
+* boot 是启动分区, 0-512字节
--- mkimg.go
+++ mkimg.go
@@ -8,9 +8,42 @@ import (
 )
 
 type config struct {
-	Output    string `yaml:"output"`
-	Boot      string `yaml:"boot"`
-	BootBytes []byte `yaml:"-"`
+	Output     string   `yaml:"output"`
+	OutputFile *os.File `yaml:"-"`
+	Boot       string   `yaml:"boot"`
+}
+
+func (c *config) TruncateOutput() (err error) {
+	c.OutputFile, err = os.Create(Config.Output)
+	if err != nil {
+		return err
+	}
+
+	err = c.OutputFile.Truncate(size)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *config) WriteBoot() (err error) {
+	content, err := ioutil.ReadFile(c.Boot)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.OutputFile.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.OutputFile.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 var Config config
@@ -18,7 +51,7 @@ var Config config
 // 1.44mb
 var size = int64(1024 * 1440)
 
-func main() {
+func init() {
 	content, err := ioutil.ReadFile("mkimg.yml")
 	if err != nil {
 		panic(err)
@@ -28,29 +61,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
 
-	content, err = ioutil.ReadFile(Config.Boot)
+func main() {
+
+	err := Config.TruncateOutput()
 	if err != nil {
 		panic(err)
 	}
 
-	Config.BootBytes = content
-
-	output, err := os.Create(Config.Output)
-	if err != nil {
-		panic(err)
-	}
-
-	err = output.Truncate(size)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = output.Seek(0, 0)
-	if err != nil {
-		panic(err)
-	}
-	_, err = output.Write(Config.BootBytes)
+	err = Config.WriteBoot()
 	if err != nil {
 		panic(err)
 	}
